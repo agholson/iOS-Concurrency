8d8ae7e3d5649c822784008ed6772b67bf83290a
agholson <87455899+agholson@users.noreply.github.com>
2022-11-12 15:34:17 -0500

    Initial Commit

---
+++ iOS Concurrency/iOS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ConcurrencyApp.swift
+//  iOS Concurrency
+//
+//  Created by Leone on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
